db0bab7b81c895e3e3f65ae062e6b9fe380088cc
herlinpriatna <herlinpriatna@student.telkomuniversity.ac.id>
2023-03-21 16:04:13 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- modul5_1302213018/modul5_1302213018/Program.cs
+++ modul5_1302213018/modul5_1302213018/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+    {
+        dynamic jml = input1;
+        dynamic jml2 = input2;
+        dynamic jml3 = input3;
+
+        return jml + jml2+ jml3;
+    }
+}
