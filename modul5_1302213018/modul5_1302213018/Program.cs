7306185e74421e0497a753180dc09607731600fa
herlinpriatna <herlinpriatna@student.telkomuniversity.ac.id>
2023-03-21 16:53:03 +0700

    menambahkan class SimpleDataBase

--- modul5_1302213018/modul5_1302213018/Program.cs
+++ modul5_1302213018/modul5_1302213018/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+public class Program {
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<int> data = new SimpleDataBase<int>();
+        data.AddNewData(13);
+        data.AddNewData(02);
+        data.AddNewData(21);
+        data.PrintAllData();
+    }
+
+
+}
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+    public void AddNewData(T newdata)
+    {
+  
+    this.inputDates.Add(DateTime.Now);
+    this.storedData.Add(newdata);
+    }
+    public void PrintAllData()
+    {
+        for(int i = 0; i < this.inputDates.Count; i++)
+        {
+            Console.WriteLine("Data " + i + " berisi " + this.storedData[i] + " yang disimpan pada waktu UTC : " + this.inputDates[i]);
+        }
+    }
+}
+
+
